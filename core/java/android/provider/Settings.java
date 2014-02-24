f072a3a2b3915330bfe5a7aa0bb1473a9a2f1cb4
hany <hany.alsamman@gmail.com>
2014-02-24 03:19:35 +0000

    XuiMod: Toast Animations [1/2]
    Signed-off-by: vibhu0009 <cvibhu0009@gmail.com>
    
    Add Xylon, Xperia Z1, TokoROM Transition and more
    
    https://github.com/zst123/XuiMod/commit/8fc1152fb485ed5e007ac8d4ea9ad7b7f1c493d8
    
    https://github.com/zst123/XuiMod/commit/1989a90850e5491799620e1f86fcbc9ae54f5161
    
    https://github.com/zst123/XuiMod/commit/e3383f127244822b9fc61db169b59e4a502d127b
    
    Credit zst123, thanks for GwonHyeok

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3986,6 +3986,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Toast Animations
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
