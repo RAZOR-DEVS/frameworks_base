2c3f061dba0642833110cc84bb8d5dff15c5ce1d
maxwen <max.weninger@gmail.com>
2015-07-31 22:04:19 +0200

    [2/2] Base: Add lockscreen rotation as an optional rotation setting
    Based on :
    
    https://github.com/omnirom/android_frameworks_base/commit/297376b985fe5ba858e00f0a5cae65b8c61dc854
    
    By :  Juan Ezquerro LLanes <arrase@gamil.com> / maxwen <max.weninger@gmail.com>
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    Change-Id: I306f758fedd0564977e9bc203360a088f2e95e1a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -535,6 +535,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.POLICY_CONTROL), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_ROTATION),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ACCELEROMETER_ROTATION),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -638,6 +644,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mNavigationBarView.updateNavigationBarSettings();
                     mNavigationBarView.onNavButtonTouched();
                 }
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_ROTATION))
+                    || uri.equals(Settings.System.getUriFor(
+                    Settings.System.ACCELEROMETER_ROTATION))) {
+                mStatusBarWindowManager.updateKeyguardScreenRotation();
             }
         }
 
